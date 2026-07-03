ac2876a1d550d8d112597801783f4820b629d8ea
kauanns2 <kauanns2006@gmail.com>
2026-07-03 06:37:34 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
 
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
