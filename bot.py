096433e4accddc444a3d7f68ccc97955381d42ea
kauanns2 <kauanns2006@gmail.com>
2026-07-03 07:42:56 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,8 +10,8 @@ intents.message_content = True
 
 bot = commands.Bot(
     command_prefix="!",
-    intents=intents
-)
+    intents=intents)
+
 @bot.event
 async def on_ready():
     print(f"{bot.user} está online!")
